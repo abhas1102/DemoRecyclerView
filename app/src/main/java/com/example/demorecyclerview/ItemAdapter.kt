ea85f04ed7b1f3b0a0e134c0ccfb4289415d6a31
Abhas Kumar <abhas1102@gmail.com>
2021-08-02 23:01:32 +0530

    Created ItemAdapter adapter class to attach it with Recycler View.

--- app/src/main/java/com/example/demorecyclerview/ItemAdapter.kt
+++ app/src/main/java/com/example/demorecyclerview/ItemAdapter.kt
@@ -1,20 +1,30 @@
 package com.example.demorecyclerview
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.items_row.view.*
 
 class ItemAdapter(val context:Context, val items:ArrayList<String>):RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.items_row,parent,false))
     }
 
+
+
     override fun onBindViewHolder(holder: ItemAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        holder.tvItem.text = item
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
+    }
+
+    class ViewHolder(view:View):RecyclerView.ViewHolder(view) {
+        val tvItem = view.tvItem
     }
 
 }
--- app/src/main/java/com/example/demorecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/demorecyclerview/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.demorecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemAdapter = ItemAdapter(this,getItemsList())
+        rvContainer.adapter = itemAdapter
+    }
+
+    private fun getItemsList():ArrayList<String>{
+        val list = ArrayList<String>()
+        list.add("Abhas Kumar")
+        list.add("Abhishek Bundela")
+        list.add("Adarsh Singh Chauhan")
+        list.add("Akash singh")
+        list.add("Anita Akhileshwar")
+        list.add("Ariga simha Teja")
+        list.add("Amit Kumar Yadav")
+        list.add("Bablu kumar")
+        list.add("Bhavishya Goel")
+        list.add("Chirag Gupta")
+        list.add("Dharmveer Sharma")
+        list.add("Komuraveli Prasanth")
+        list.add("Karan Sapolia")
+        list.add("Khusboo Gutpta")
+
+        return list
     }
 }
