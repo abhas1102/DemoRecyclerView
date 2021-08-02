13f5cb48ccdbe68dcfb2a69553e773107e98a515
Abhas Kumar <abhas1102@gmail.com>
2021-08-02 21:30:46 +0530

    Layout for row items created

---
+++ app/src/main/java/com/example/demorecyclerview/ItemAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.demorecyclerview
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(val context:Context, val items:ArrayList<String>):RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
